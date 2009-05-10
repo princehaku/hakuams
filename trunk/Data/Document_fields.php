0219db8de397e756bf589a59d888896a77d67d3d
baizhongwei <baizhongwei@80c69534-3d09-11de-8fef-1f5ae417a2df>
2009-05-10 04:23:03 +0000

    简化了下..删掉了多余东西

---
+++ trunk/Data/Document_fields.php
@@ -0,0 +1,18 @@
+<?php
+if (!defined('THINK_PATH')) exit();
+//-00000000001
+return array (
+  0 => 'id',
+  1 => 'cateid',
+  2 => 'title',
+  3 => 'content',
+  4 => 'shortcontent',
+  5 => 'picurl',
+  6 => 'writer',
+  7 => 'data',
+  8 => 'exam',
+  9 => 'ispic',
+  '_autoInc' => true,
+  '_pk' => 'id',
+);
+?>
---
+++ trunk/Data/State_fields.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('THINK_PATH')) exit();
+//-00000000001
+return array (
+  0 => 'user',
+  1 => 'ltime',
+  '_autoInc' => false,
+);
+?>
