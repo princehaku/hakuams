f8f8982eca5efe806e1c4bf2cb66cc36f77ba04b
baizhongwei@gmail.com <baizhongwei@gmail.com@80c69534-3d09-11de-8fef-1f5ae417a2df>
2009-11-14 07:02:33 +0000

    加入文件上传功能

--- Conf/config.php
+++ Conf/config.php
@@ -28,12 +28,14 @@ $config=array(
 	'ARTNUMS'               =>17,
 	/*在这个时间范围内登陆只算一次*/
 	'LGTIME'				=>3600,
-	/*图片上传目录(注意php的设置是否可以跨htdocs)*/
-	'DIR'                   =>'D:/Program Files/server/htdocs/hakuams/uploads',
+	/*图片和文件的上传目录(注意php的设置是否可以跨htdocs)*/
+	'DIR'                   =>'D:/htdocs/hakuams/uploads',
 	/*图片显示的路径设置(用于后台缩略图的正确显示)*/
 	'SPICURL'                   =>'/hakuams/uploads/',
 	/*允许上传的图片类型*/
-	'UPLOAD_ALLOWEXTS'      =>'jpg|jpge|gif|bmp',
+	'PIC_ALLOWEXTS'      =>'jpg|jpge|gif|bmp',
+	/*允许上传的文件类型*/
+	'UPLOAD_ALLOWEXTS'      =>'sql|doc',
 	/*文档类型*/
 	'ARCTYPE'               =>array(0=>'标准',1=>'图文',2=>'图集'),
 	/*普通用户的菜单*/
--- Js/main.js
+++ Js/main.js
@@ -10,20 +10,20 @@ blog  http://hi.baidu.com/baizhongwei(主)
 //提交文章时的判断
 function subdoc()
 {
-	if($('category').value==""|$("title").value=="")
+	if($$('category').value==""||$$("title").value=="")
 	{
-		if($('category').value=="")
+		if($$('category').value=="")
 			{
-				alert("请选择一个目录");
+				$.prompt("请选择一个目录");
 				}
 			else
 			{
-				alert("请输入一个标题");
+				$.prompt("请输入一个标题");
 				}
 		}
 	else
 	{
-		$('article').submit();
+		$$('article').submit();
 		}
 	
 	}
@@ -33,16 +33,16 @@ function upinfo()
 
 	for(i=0;i<=40;i++)
 	{
-		if($('cat'+i).checked)
+		if($$('cat'+i).checked)
 		{
 			
-			$('alcat').value += $('cat'+i).value; 
-			$('alcat').value += "|"; 
+			$$('alcat').value += $$('cat'+i).value; 
+			$$('alcat').value += "|"; 
 			
 			}
 	}
 	
-	$('reg').submit();
+	$$('reg').submit();
 	
 }
 
